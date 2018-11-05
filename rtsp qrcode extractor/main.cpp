9a456eccb772fe009a157a9a78ffe8e54d1e3382
Piyush Patel <36793743+da-piyushpatel@users.noreply.github.com>
2018-11-05 23:54:27 +0530

    Add files via upload

---
+++ rtsp qrcode extractor/main.cpp
@@ -0,0 +1,88 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <zbar.h>
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+using namespace cv;
+using namespace zbar;
+
+int main(int argc, char **argv) {
+	//For webcam feed use 0 or 1 instead of url
+	//replace your url in here
+    // VideoCapture cap("rtsp://192.168.42.1/live");
+    cv::VideoCapture cap(0);
+
+    if (!cap.isOpened()) {
+        cerr << "Could not open camera." << endl;
+        exit(EXIT_FAILURE);
+    }
+
+    cv::namedWindow("captured", CV_WINDOW_AUTOSIZE);
+    
+    // Create a zbar reader
+    ImageScanner scanner;
+    
+    // Configure the reader
+    scanner.set_config(ZBAR_NONE, ZBAR_CFG_ENABLE, 1);
+
+    while(1) {
+        // Capture an OpenCV frame
+        cv::Mat frame, frame_grayscale;
+        cap >> frame;
+
+        // Convert to grayscale
+        cvtColor(frame, frame_grayscale, CV_BGR2GRAY);
+
+        // Obtain image data
+        int width = frame_grayscale.cols;
+        int height = frame_grayscale.rows;
+        uchar *raw = (uchar *)(frame_grayscale.data);
+
+        // Wrap image data
+        Image image(width, height, "Y800", raw, width * height);
+
+        // Scan the image for barcodes
+        scanner.scan(image);
+
+        // Extract results
+        int counter = 0;
+        for (Image::SymbolIterator symbol = image.symbol_begin(); symbol != image.symbol_end(); ++symbol) {
+            time_t now;
+            tm *current;
+            now = time(0);
+            current = localtime(&now);
+
+            // do something useful with results
+            cout    << "[" << current->tm_hour << ":" << current->tm_min << ":" << setw(2) << setfill('0') << current->tm_sec << "] " << counter << " "
+                    << "decoded " << symbol->get_type_name()
+                    << " symbol \"" << symbol->get_data() << '"' << endl;
+
+            //cout << "Location: (" << symbol->get_location_x(0) << "," << symbol->get_location_y(0) << ")" << endl;
+            cout << "Size: " << symbol->get_location_size() << endl;
+            
+            // Draw location of the symbols found
+            if (symbol->get_location_size() == 4) {
+                //rectangle(frame, Rect(symbol->get_location_x(i), symbol->get_location_y(i), 10, 10), Scalar(0, 255, 0));
+                line(frame, Point(symbol->get_location_x(0), symbol->get_location_y(0)), Point(symbol->get_location_x(1), symbol->get_location_y(1)), Scalar(0, 255, 0), 2, 8, 0);
+                line(frame, Point(symbol->get_location_x(1), symbol->get_location_y(1)), Point(symbol->get_location_x(2), symbol->get_location_y(2)), Scalar(0, 255, 0), 2, 8, 0);
+                line(frame, Point(symbol->get_location_x(2), symbol->get_location_y(2)), Point(symbol->get_location_x(3), symbol->get_location_y(3)), Scalar(0, 255, 0), 2, 8, 0);
+                line(frame, Point(symbol->get_location_x(3), symbol->get_location_y(3)), Point(symbol->get_location_x(0), symbol->get_location_y(0)), Scalar(0, 255, 0), 2, 8, 0);
+            }
+            
+            counter++;
+        }
+
+        // Show captured frame, now with overlays!
+        imshow("captured", frame);
+                                                                                                                                                          
+        // clean up
+        image.set_data(NULL, 0);
+        
+        waitKey(30);
+    }
+
+    return 0;
+}
